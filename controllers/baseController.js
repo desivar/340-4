e64dbb1591ccc7baab1e7723130dee33e4786718
desivar <jillian_desire@yahoo.es>
2025-04-14 13:21:04 -0600

    base contrl modf

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,10 +1,16 @@
 const utilities = require("../utilities/")
 const baseController = {}
 
-baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  // req.flash("notice", "This is a flash message.")
-  res.render("index", {title: "Home", nav})
-}
 
+
+baseController.buildHome = async function(req, res){
+  try {
+      const nav = await utilities.getNav();
+      res.render("index", {title: "Home", nav});
+  } catch (error) {
+      console.error("Error building home:", error);
+      // Handle error (e.g., render an error page)
+      res.render("errors/error", {message: "Failed to load home page."});
+  }
+}
 module.exports = baseController
