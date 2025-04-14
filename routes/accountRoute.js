3ddc3fd807c8b849114d112a86906f2fd5978cc6
desivar <jillian_desire@yahoo.es>
2025-04-14 11:01:15 -0600

    fix error in account Rote

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,29 +1,79 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const accountCont = require("../controllers/accountController")
-const utilities = require("../utilities")
+// Needed Resources
+const express = require('express')
+const router = new express.Router()
+const utilities = require('../utilities/')
+const accountController = require('../controllers/accountController')
 const regValidate = require('../utilities/account-validation')
 
-// Route to build login view
-router.get("/login", utilities.handleErrors(accountCont.buildLogin));
-// Route to build register view
-router.get("/register", utilities.handleErrors(accountCont.buildRegister));
-// Post new account information
+/* ***********************
+ * Route to build Login View
+ *************************/
+router.get('/login', utilities.handleErrors(accountController.buildLogin))
+
+/* ***********************
+ * Process the login attempt
+ *************************/
 router.post(
-    '/register', 
-    regValidate.registationRules(),
-    regValidate.checkRegData,
-    utilities.handleErrors(accountCont.registerAccount)
-)
-// Process the login attempt
-router.post(
-    "/login",
+    '/login',
     regValidate.loginRules(),
     regValidate.checkLoginData,
-    (req, res) => {
-      res.status(200).send('login process')
-    }
-  )
+    utilities.handleErrors(accountController.accountLogin)
+)
 
-module.exports = router;
+/* ***********************
+ * Route to build Registration View
+ *************************/
+router.get('/registration', utilities.handleErrors(accountController.buildRegistration))
+
+/* ***********************
+ * Route to handle Registration
+ *************************/
+router.post(
+    '/registration',
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+)
+
+/* ***********************
+ * Route to build Account Management View
+ *************************/
+router.get('/', utilities.checkLogin, utilities.handleErrors(accountController.buildManagement))
+
+/* ***********************
+ * Route to build Update Account View
+ *************************/
+router.get(
+    '/update/:account_id',
+    utilities.checkLogin,
+    utilities.handleErrors(accountController.buildUpdateAccountView)
+)
+
+/* ***********************
+ * Update Account Information
+ *************************/
+router.post(
+    '/update-user-info/',
+    utilities.checkLogin,
+    regValidate.updateRegistrationRules(),
+    regValidate.checkUpdateRegData,
+    utilities.handleErrors(accountController.updateAccountInfo)
+)
+
+/* ***********************
+ * Change Password
+ *************************/
+router.post(
+    '/update-user-password/',
+    utilities.checkLogin,
+    regValidate.updatePasswordRules(),
+    regValidate.checkPasswordData,
+    utilities.handleErrors(accountController.updatePassword)
+)
+
+/* ***********************
+ * Route to build Logout View
+ *************************/
+router.get("/logout", utilities.handleErrors(accountController.logout));
+
+module.exports = router
