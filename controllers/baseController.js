a34bba968b6c9aed241d0f7e980f4c74faff5a4c
desivar <jillian_desire@yahoo.es>
2025-04-14 15:54:32 -0600

    fix error

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,16 +1,10 @@
 const utilities = require("../utilities/")
 const baseController = {}
 
-
-
 baseController.buildHome = async function(req, res){
-  try {
-      const nav = await utilities.getNav();
-      res.render("index", {title: "Home", nav});
-  } catch (error) {
-      console.error("Error building home:", error);
-      // Handle error (e.g., render an error page)
-      res.render("errors/error", {message: "Failed to load home page."});
-  }
+  const nav = await utilities.getNav()
+  // req.flash("notice", "This is a flash message.")
+  res.render("index", {title: "Home", nav})
 }
+
 module.exports = baseController
