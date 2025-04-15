b84631b6837d063595f45fe76c812b01bddec2d8
desivar <jillian_desire@yahoo.es>
2025-04-15 08:34:27 -0600

    invRout

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,69 +1,61 @@
-// Needed Resources
-const express = require('express')
-const router = new express.Router()
-const invController = require('../controllers/invController')
-const revController = require('../controllers/reviewController')
-const utilities = require('../utilities/')
-const regValidate = require('../utilities/inventory-validation')
+const express = require("express");
+const router = new express.Router();
+const invController = require("../controllers/invController");
+const classificationValidate = require("../utilities/classification-validation");
+const inventoryValidate = require("../utilities/inventory-validation");
+const utilities = require("../utilities");
+
+// NEW: Management View Route
+router.get("/add-inventory", utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(invController.buildAddInventory));
+router.get("/", utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(invController.buildManagement));
+router.get("/add-classification", utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(invController.buildAddClassification));
+
+// Route to build vehicle detail view
+router.get("/detail/:inv_id", (req, res, next) => {
+    console.log("Detail route hit for inv_id:", req.params.inv_id);
+    next();
+}, utilities.handleErrors(invController.buildByInventoryId)); // Changed from buildDetailView to buildByInventoryId
 
 // Route to build inventory by classification view
-router.get('/type/:classificationId', utilities.handleErrors(invController.buildByClassificationId))
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
-// Route to build details by InventoryID view
-router.get('/detail/:inventoryId', utilities.handleErrors(invController.buildByInventoryID))
+// Route to get inventory by classification as JSON for AJAX
+router.get("/getInventory/:classification_id", utilities.handleErrors(invController.getInventoryJSON));
 
-// Route to build Inventory Management view
-router.get('/', utilities.checkAccountType, utilities.handleErrors(invController.buildByInvManagement))
+// Route to build edit inventory view
+router.get("/edit/:inv_id", utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(invController.buildEditInventoryView));
 
-// Route to build Add Classification View
-router.get('/add-classification', utilities.handleErrors(invController.buildByAddClassification))
+// Route to build delete confirmation view
+router.get("/delete/:inv_id", utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(invController.buildDeleteConfirmView));
 
-// Route to handle Add Classification
-router.post(
-    '/add-classification',
-    regValidate.classificationRules(),
-    regValidate.checkClassificationData,
-    utilities.handleErrors(invController.addClassification)
-)
+// Route to delete inventory item
+router.post("/delete", utilities.checkLogin, utilities.checkAccountType, utilities.handleErrors(invController.deleteInventory));
 
-// Route to build Add Inventory View
-router.get('/add-inventory', utilities.handleErrors(invController.buildByAddInventory))
+// Route to update inventory item
+router.post("/update/",
+  utilities.checkLogin,
+  utilities.checkAccountType,
+  inventoryValidate.inventoryRules(),
+  inventoryValidate.checkUpdateData,
+  utilities.handleErrors(invController.updateInventory)
+);
 
-// Route to handle Add Inventory
-router.post(
-    '/add-inventory',
-    regValidate.inventoryRules(),
-    regValidate.checkInventoryData,
-    utilities.handleErrors(invController.addInventory)
-)
+// Route to build add vehicle view
+router.post("/add-classification",
+  utilities.checkLogin,
+  utilities.checkAccountType,
+  classificationValidate.classificationRules(),
+  classificationValidate.checkClassificationData,
+  utilities.handleErrors(invController.addClassification)
+);
 
-// Route to build Get Inventory View
-router.get(
-    '/getInventory/:classification_id',
-    utilities.checkAccountType,
-    utilities.handleErrors(invController.getInventoryJSON)
-)
+// Route to build add vehicle view
+router.post("/add-inventory",
+  utilities.checkLogin,
+  utilities.checkAccountType,
+  inventoryValidate.inventoryRules(),
+  inventoryValidate.checkInventoryData,
+  utilities.handleErrors(invController.addInventory)
+);
 
-// Route to build Edit Inventory View
-router.get('/edit/:inventoryId', utilities.checkAccountType, utilities.handleErrors(invController.buildByEditInventory))
-
-// Route to handle Edit/Update Inventory
-router.post(
-    '/update/',
-    regValidate.inventoryRules(),
-    regValidate.checkInventoryData,
-    utilities.handleErrors(invController.updateInventory)
-)
-
-// Route to build Delete Inventory View
-router.get(
-    '/delete/:inventoryId',
-    utilities.checkAccountType,
-    utilities.handleErrors(invController.buildByDeleteInventory)
-)
-
-// Route to handle Delete Inventory
-router.post('/delete/', utilities.handleErrors(invController.deleteInventory))
-
-
-module.exports = router
+module.exports = router;
