27a52ff245a6d8db110021eb4dd242c686c4e601
desivar <jillian_desire@yahoo.es>
2025-04-15 08:32:06 -0600

    update accountRot

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,79 +1,75 @@
-// Needed Resources
-const express = require('express')
-const router = new express.Router()
-const utilities = require('../utilities/')
-const accountController = require('../controllers/accountController')
-const regValidate = require('../utilities/account-validation')
+// This file defines the routes for account management, including login, registration, and account updates.
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const utilities = require("../utilities");
+const validate = require('../utilities/account-validation');
 
-/* ***********************
- * Route to build Login View
- *************************/
-router.get('/login', utilities.handleErrors(accountController.buildLogin))
+// Route to deliver the login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
-/* ***********************
- * Process the login attempt
- *************************/
-router.post(
-    '/login',
-    regValidate.loginRules(),
-    regValidate.checkLoginData,
-    utilities.handleErrors(accountController.accountLogin)
-)
+// Route to build registration view
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
-/* ***********************
- * Route to build Registration View
- *************************/
-router.get('/registration', utilities.handleErrors(accountController.buildRegistration))
+// Route to deliver the account management view
+router.get("/", utilities.checkLogin, utilities.handleErrors(accountController.buildManagement));
 
-/* ***********************
- * Route to handle Registration
- *************************/
-router.post(
-    '/registration',
-    regValidate.registrationRules(),
-    regValidate.checkRegData,
-    utilities.handleErrors(accountController.registerAccount)
-)
+// Route to deliver the account update view
+router.get("/update/:account_id", utilities.checkLogin, utilities.handleErrors(accountController.buildUpdateView));
 
-/* ***********************
- * Route to build Account Management View
- *************************/
-router.get('/', utilities.checkLogin, utilities.handleErrors(accountController.buildManagement))
-
-/* ***********************
- * Route to build Update Account View
- *************************/
-router.get(
-    '/update/:account_id',
-    utilities.checkLogin,
-    utilities.handleErrors(accountController.buildUpdateAccountView)
-)
-
-/* ***********************
- * Update Account Information
- *************************/
-router.post(
-    '/update-user-info/',
-    utilities.checkLogin,
-    regValidate.updateRegistrationRules(),
-    regValidate.checkUpdateRegData,
-    utilities.handleErrors(accountController.updateAccountInfo)
-)
-
-/* ***********************
- * Change Password
- *************************/
-router.post(
-    '/update-user-password/',
-    utilities.checkLogin,
-    regValidate.updatePasswordRules(),
-    regValidate.checkPasswordData,
-    utilities.handleErrors(accountController.updatePassword)
-)
-
-/* ***********************
- * Route to build Logout View
- *************************/
+// Route to handle logout
 router.get("/logout", utilities.handleErrors(accountController.logout));
 
-module.exports = router
+// Process the registration data
+router.post(
+    "/register",
+    validate.registationRules(), 
+    validate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+);
+
+// Process the login request
+router.post(
+    "/login",
+    validate.loginRules(), 
+    validate.checkLoginData,
+    utilities.handleErrors(accountController.accountLogin)
+);
+
+// Process the account update
+router.post(
+    "/update",
+    utilities.checkLogin,
+    validate.accountUpdateRules(), 
+    validate.checkAccountUpdateData,
+    utilities.handleErrors(accountController.updateAccount)
+);
+
+// Process the password update
+router.post(
+    "/update-password",
+    utilities.checkLogin,
+    validate.passwordUpdateRules(), 
+    validate.checkPasswordUpdateData,
+    utilities.handleErrors(accountController.updatePassword)
+);
+
+// Process adding a vehicle to favorites
+router.post(
+    "/add-favorite",
+    utilities.checkLogin,
+    validate.favoritesRules(), 
+    validate.checkFavoritesData,
+    utilities.handleErrors(accountController.addFavorite)
+);
+
+// Process removing a vehicle from favorites
+router.post(
+    "/remove-favorite",
+    utilities.checkLogin,
+    validate.favoritesRules(), 
+    validate.checkFavoritesData,
+    utilities.handleErrors(accountController.removeFavorite)
+);
+
+module.exports = router;
