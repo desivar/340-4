379e20c7a6fc320d79a70ebb9fc3aa7178e26786
desivar <jillian_desire@yahoo.es>
2025-04-15 08:35:16 -0600

    static

--- routes/static.js
+++ routes/static.js
@@ -1,10 +1,16 @@
-// Needed Resources
 const express = require('express');
 const router = express.Router();
 
-// Set up "public" folder for static files
+// Static Routes
+// Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
+router.use("/css", express.static(__dirname + "public/css"));
+router.use("/js", express.static(__dirname + "public/js"));
+router.use("/images", express.static(__dirname + "public/images"));
 
 module.exports = router;
+
+
+
 
 
