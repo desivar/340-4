e2d6b2d803781a88886c4985667c7394739d5790
desivar <jillian_desire@yahoo.es>
2025-04-15 08:08:57 -0600

    fix typoin error contrl

--- controllers/errorTestController.js.js
+++
@@ -1,9 +0,0 @@
-const utilities = require('../utilities')
-
-const errorTestController = {}
-
-errorTestController.triggerError = async function (req, res, next) {
-    throw new Error('This is a test error.')
-}
-
-module.exports = errorTestController
