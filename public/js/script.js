29c446b7fe335289682384ab1076ce3c060ca606
desivar <jillian_desire@yahoo.es>
2025-04-15 22:50:41 -0600

    script

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('own-today-btn').addEventListener('click', function() {
+    window.location.href = '/products/dmc-delorean.html'; // Redirect to the page
+    // Or:
+    // window.open('/products/dmc-delorean.html', '_blank'); // Open in a new tab
+});
