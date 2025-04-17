1596f49caeab9c6c5bef5763b31ae6c5eddb28bc
desivar <jillian_desire@yahoo.es>
2025-04-17 09:04:20 -0600

    update

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,10 +1,8 @@
-
 const utilities = require("../utilities/")
 const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
-  // INSERT THIS LINE --> console.log("Data from utilities.getNav():", nav);
   //req.flash("notice", "This is a flash message.") // Example flash message
   res.render("index", {title: "Home", nav})
 }
