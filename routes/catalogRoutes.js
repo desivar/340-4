04d7a02bb02d42f9ac19ea04695b021d115fc955
desivar <jillian_desire@yahoo.es>
2025-04-18 08:26:35 -0600

    delete catalogue

--- routes/catalogRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const catalogController = require("../controllers/catalogController"); // You'll need to create this controller
-
-// Route to display the vehicle catalog
-router.get("/vehicles", catalogController.buildCatalog); // Example path: /vehicles
-
-module.exports = router;
